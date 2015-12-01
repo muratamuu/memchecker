dde889ff811c7b01ac4d5676de9d3ef1a6aa5b06
muratamuu <muratamuu@gmail.com>
2015-12-01 18:52:13 +0900

    realloc hook

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 static void*(*org_malloc_hook)(size_t, const void*);
 static void*(*org_realloc_hook)(void*, size_t, const void*);
-//static void*(*org_memalign_hook)(size_t, size_t, const void*);
-//static void(*org_free_hook)(void*, const void*);
+static void*(*org_memalign_hook)(size_t, size_t, const void*);
+static void(*org_free_hook)(void*, const void*);
 
 int main()
 {
@@ -30,12 +30,34 @@ static void* realloc_hook(void* p, size_t size, const void* ra)
   return p;
 }
 
-void init_hook(void)
+void* memalign_hook(size_t alignment, size_t size, const void* ra)
 {
-  org_malloc_hook  = __malloc_hook;
-  __malloc_hook    = malloc_hook;
-  org_realloc_hook = __realloc_hook;
-  __realloc_hook   = realloc_hook;
+  __memalign_hook = org_memalign_hook;
+  void* p = memalign(alignment, size);
+  __memalign_hook = memalign_hook;
+  return p;
 }
 
+void free_hook(void* p, const void* ra)
+{
+  __free_hook = org_free_hook;
+  free(p);
+  __free_hook = free_hook;
+  return;
+}
+
+void init_hook(void)
+{
+  org_malloc_hook   = __malloc_hook;
+  __malloc_hook     = malloc_hook;
+  org_realloc_hook  = __realloc_hook;
+  __realloc_hook    = realloc_hook;
+  org_memalign_hook = __memalign_hook;
+  __memalign_hook   = memalign_hook;
+  org_free_hook     = __free_hook;
+  __free_hook       = free_hook;
+}
+
+extern "C" {
 void (* volatile __malloc_initialize_hook)(void) = init_hook;
+}
